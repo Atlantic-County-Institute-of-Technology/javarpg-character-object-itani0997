21ea2a6b61a3e019a5cadabc52197af92e97235f
itani0997 <isla9190@student.acitech.org>
2026-02-05 12:50:48 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        Dice.Die die = new Dice.Die();
+        System.out.println(dice);
+        System.out.println(die);
+        System.out.println("Placeholder.");
+    }}
+
