d2a763d0718565ce9e635f7a454801468a34e5a0
itani0997 <isla9190@student.acitech.org>
2026-02-09 12:21:11 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Dice dice = new Dice();
-        Dice.Die die = new Dice.Die();
-        System.out.println(dice);
-        System.out.println(die);
-        System.out.println("Placeholder.");
-    }}
+        System.out.println("Java RPG Character Test");
 
+        RPG character = new RPG("Adventurer", 1, "melee");
+        character.rollStats();
+        System.out.println(character);
+    }
+}
