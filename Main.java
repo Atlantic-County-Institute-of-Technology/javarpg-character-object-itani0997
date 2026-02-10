930a63851a9ce0f04600ce3575ddf430b223d8aa
itani0997 <isla9190@student.acitech.org>
2026-02-10 13:42:09 -0500

    Final Commit
    Added Dice functionality and updated character initialization.

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        RPG character = new RPG("TestCharacter", 1, "melee");
+        Dice dice = new Dice();
+        Dice.Die die = new Dice.Die(6); 
+        System.out.println(dice);
+        System.out.println(die);
+        System.out.println("Placeholder"); 
+
+        RPG character = new RPG ("ALEX", 1, "melee"); 
         character.rollStats();
-        System.out.println(character);
-        System.out.println("Alive: " + character.isAlive());
+        System.out.println("\n" + character); 
     }
 }
